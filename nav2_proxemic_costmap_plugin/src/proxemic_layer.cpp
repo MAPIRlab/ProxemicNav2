6c3bdf78a427bf2bde9077818556e6bd6442d9d3
martaloring <94187225+martaloring@users.noreply.github.com>
2023-01-20 13:07:19 +0100

    Add files via upload

--- nav2_proxemic_costmap_plugin/src/proxemic_layer.cpp
+++ nav2_proxemic_costmap_plugin/src/proxemic_layer.cpp
@@ -11,16 +11,11 @@ using nav2_costmap_2d::NO_INFORMATION;
 namespace nav2_proxemic_costmap_plugin
 {
 
-// ProxemicLayer::ProxemicLayer(): last_min_x_(-std::numeric_limits<float>::max()),last_min_y_(-std::numeric_limits<float>::max()),last_max_x_(std::numeric_limits<float>::max()),last_max_y_(std::numeric_limits<float>::max()){
-// }
-
 ProxemicLayer::~ProxemicLayer() {}
 
 void ProxemicLayer::onInitialize()
 {
     auto node = node_.lock(); //node_ (weak_ptr), node (shared_ptr)
-
-    //RCLCPP_INFO(node->get_logger(),"adios\n");
 
     sub_ = node->create_subscription<geometry_msgs::msg::PoseStamped>("/people_topic",10,std::bind(&ProxemicLayer::peopleCallBack, this, std::placeholders::_1));
 
@@ -43,7 +38,6 @@ void ProxemicLayer::onInitialize()
     pose_.header.stamp.nanosec = 0;
 
     global_frame_ = layered_costmap_->getGlobalFrameID();
-    //rolling_window_ = layered_costmap_->isRolling();
     rolling_window_ = false;
 
     ProxemicLayer::matchSize();
@@ -56,10 +50,6 @@ void ProxemicLayer::onInitialize()
     layered_costmap_->getCostmap()->getOriginY());
 
     proxemic_costmap_->setDefaultValue(nav2_costmap_2d::FREE_SPACE);
-
-    //costmap_pub_ = std::make_shared<Costmap2DPublisher>(node_, proxemic_costmap_.get(), global_frame_, name_ + "/costmap", true);
-    //costmap_pub_->on_activate();
-
 }
 
 void ProxemicLayer::getFrameNames()
@@ -97,12 +87,10 @@ void ProxemicLayer::peopleCallBack(const geometry_msgs::msg::PoseStamped::Shared
         
     pose_.pose.position.x = msg->pose.position.x;
     pose_.pose.position.y = msg->pose.position.y;
-    pose_.pose.orientation.w = msg->pose.orientation.w;
+    pose_.pose.orientation.w = msg->pose.orientation.z;
 
     auto node = node_.lock();
-    RCLCPP_INFO(node->get_logger(),"He recibido la pose: [x:%f, y:%f, o:%f]", pose_.pose.position.x, pose_.pose.position.y, pose_.pose.orientation.w);
-
-    //proxemic_costmap_->setDefaultValue(nav2_costmap_2d::FREE_SPACE);
+    RCLCPP_INFO(node->get_logger(),"He recibido la pose: [x:%f, y:%f, o:%f]", pose_.pose.position.x, pose_.pose.position.y, pose_.pose.orientation.z);
 
     need_recalculation_ = true;
     update_cost_ = false;
@@ -110,9 +98,7 @@ void ProxemicLayer::peopleCallBack(const geometry_msgs::msg::PoseStamped::Shared
 
 void ProxemicLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double * min_x, double * min_y, double * max_x, double * max_y){ 
     
-    //if(!enabled_){return;}
     auto node = node_.lock();
-    RCLCPP_INFO(node->get_logger(),"updatebounds");
 
     if(need_recalculation_){
         float posex = pose_.pose.position.x;
@@ -135,26 +121,8 @@ void ProxemicLayer::updateBounds(double robot_x, double robot_y, double robot_ya
         }
         need_recalculation_ = false;
         update_cost_ = true;
-        auto node = node_.lock();
-        RCLCPP_INFO(node->get_logger(),"dentro bounds");
-        //proxemic_costmap_->setDefaultValue(nav2_costmap_2d::FREE_SPACE);
-    }//else{
-
-    //     double tmp_min_x = last_min_x_;
-    //     double tmp_min_y = last_min_y_;
-    //     double tmp_max_x = last_max_x_;
-    //     double tmp_max_y = last_max_y_;
-    //     last_min_x_ = *min_x;
-    //     last_min_y_ = *min_y;
-    //     last_max_x_ = *max_x;
-    //     last_max_y_ = *max_y;
-    //     *min_x = std::min(tmp_min_x, *min_x);
-    //     *min_y = std::min(tmp_min_y, *min_y);
-    //     *max_x = std::max(tmp_max_x, *max_x);
-    //     *max_y = std::max(tmp_max_y, *max_y);
-    // }
+    }
     
-
 }
 
 void ProxemicLayer::updateCosts(nav2_costmap_2d::Costmap2D & master_grid, int min_i, int min_j, int max_i, int max_j)
@@ -163,61 +131,35 @@ void ProxemicLayer::updateCosts(nav2_costmap_2d::Costmap2D & master_grid, int mi
     if (!enabled_) {return;}
     
     auto node = node_.lock();
-    RCLCPP_INFO(node->get_logger(),"updatecosts");
 
     if(update_cost_){
-        unsigned char * master_array = master_grid.getCharMap();
-        RCLCPP_INFO(node->get_logger(),"dentro costs");
 
         float posex = pose_.pose.position.x;
         float posey = pose_.pose.position.y;
         
-        RCLCPP_INFO(node->get_logger(),"[origin_x_:%f, origin_y_:%f, resolution_:%f]", origin_x_, origin_y_, resolution_);
+        int map_x = 0;
+        int map_y = 0;
 
+        worldToMapEnforceBounds(posex, posey, map_x, map_y);
 
-        // __________________________________________________CON ESTAS LÍNEAS ME LANZA EL ERROR PARA COORDENADAS RANDOM________________________
-        // int map_x = 0;
-        // int map_y = 0;
-        //worldToMapEnforceBounds(posex, posey, map_x, map_y);
-
-
-
-        // ___________________________________CON ESTAS LÍNEAS ME LANZA EL ERROR PARA CUALQUIER COORDENADA NEGATIVA, Y PARA ALGUNAS COMO (1,3)________________________
-        unsigned int map_x = 0;
-        unsigned int map_y = 0;                                                       
-        bool bien = worldToMap(posex, posey, map_x, map_y);
-        if(!bien){
-        RCLCPP_INFO(node->get_logger(),"ERROR EN WORLD TO MAP");
-        }
-
-        
-        RCLCPP_INFO(node->get_logger(),"[map_x:%d, map_y:%d]", map_x, map_y);
-
-        max_i = map_x + 10;
-        max_j = map_y + 10;
-        min_i = map_x -10;
-        min_j = map_y - 10;
+        max_i = map_x + 5;
+        max_j = map_y + 5;
+        min_i = map_x - 5;
+        min_j = map_y - 5;
     
         RCLCPP_INFO(node->get_logger(),"Bounds after: [max_x:%d, min_x:%d, max_y:%d, min_y:%d]", max_i, min_i, max_j, min_j);
 
         // Simply computing one-by-one cost per each cell
         for (int j = min_j; j < max_j; j++) {
             for (int i = min_i; i < max_i; i++) {
-                //int index = master_grid.getIndex(i, j);
-                // setting the cost
                 unsigned char cost = LETHAL_OBSTACLE;
-                //master_array[index] = cost;
                 setCost(i, j, cost);
-                //proxemic_costmap_->setCost(i, j, cost);
             }
         }
 
         updateWithMax(master_grid, min_i, min_j, max_i, max_j);
         update_cost_ = false;
-
-        RCLCPP_INFO(node->get_logger(),"exito");
     }
-    //updateWithAddition(master_grid, min_i, min_j, max_i, max_j);
 }
 
 void ProxemicLayer::onFootprintChanged(){
