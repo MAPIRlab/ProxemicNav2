b08cadd372c56694c48f2fb16be88a95ef1781b1
martaloring <94187225+martaloring@users.noreply.github.com>
2023-03-07 14:13:56 +0100

    Add files via upload

--- nav2_proxemic_costmap_plugin/include/nav2_proxemic_costmap_plugin/proxemic_layer.hpp
+++ nav2_proxemic_costmap_plugin/include/nav2_proxemic_costmap_plugin/proxemic_layer.hpp
@@ -6,8 +6,14 @@
 #include "nav2_costmap_2d/costmap_layer.hpp"
 #include "nav2_costmap_2d/layered_costmap.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/float64.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 
 namespace nav2_proxemic_costmap_plugin
 {
@@ -15,21 +21,22 @@ namespace nav2_proxemic_costmap_plugin
 class ProxemicLayer : public nav2_costmap_2d::CostmapLayer
 {
 public:
-  ProxemicLayer();
+  ProxemicLayer()
+  {
+    costmap_ = NULL;  // this is the unsigned char* member of parent class Costmap2D.
+  }
+
+  virtual ~ProxemicLayer();
 
   virtual void onInitialize();
 
-  virtual void getFrameNames();
-
-  bool getAgentTFs(std::vector<tf2::Transform> & agents) const;
-
   virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double * min_x, double * min_y, double * max_x, double * max_y);
 
-  void peopleCallBack(const geometry_msgs::msg::Pose::SharedPtr msg);
+  void peopleCallBack(const geometry_msgs::msg::PoseArray msg);
 
   virtual void updateCosts(nav2_costmap_2d::Costmap2D & master_grid, int min_i, int min_j, int max_i, int max_j);
 
-  virtual void matchSize();
+  virtual void setGaussian(nav2_costmap_2d::Costmap2D & master_grid, double pose_x, double pose_y, double ori);
 
   virtual void onFootprintChanged();
 
@@ -39,25 +46,37 @@ public:
 
 private:
 
-  rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr sub_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr sub_;
   
   double last_min_x_, last_min_y_, last_max_x_, last_max_y_;
 
-  //geometry_msgs::msg::Pose::SharedPtr pose_;
-  geometry_msgs::msg::Pose pose_;
+  bool update_cost_;
+
+  std::vector<float> posesx;
+  std::vector<float> posesy;
+  std::vector<float> posesz;
+
+  double global_max_x, global_max_y, global_min_x, global_min_y;
+  int i_max;
+  bool nuevo;
+
 
   // Indicates that the entire gradient should be recalculated next time.
   bool need_recalculation_;
-  
-  //std::vector<std::string> agent_ids_;
-  //std::string tf_prefix_;
-  //std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  //std::string global_frame_;
 
-  // Size of gradient in cells
-  // int GRADIENT_SIZE = 20;
-  // Step of increasing cost per one cell in gradient
-  // int GRADIENT_FACTOR = 10;
+  bool rolling_window_;
+
+  std::string global_frame_;  ///< @brief The global frame for the costmap
+  std::shared_ptr<nav2_costmap_2d::Costmap2D> proxemic_costmap_;
+
+  geometry_msgs::msg::Pose new_pose;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  geometry_msgs::msg::TransformStamped robot_to_map;
+
+  //std::vector<geometry_msgs::msg::Point> points_;
+  //std::shared_ptr<nav2_costmap_2d::Costmap2DPublisher> costmap_pub_{nullptr};
+
 };
 
 }  // namespace nav2_proxemic_costmap_plugin
