ec0766478cfdc6f21d6f40ff9ff1f4815e25a44f
martaloring <94187225+martaloring@users.noreply.github.com>
2023-03-07 14:13:37 +0100

    Add files via upload

--- nav2_proxemic_costmap_plugin/src/proxemic_layer.cpp
+++ nav2_proxemic_costmap_plugin/src/proxemic_layer.cpp
@@ -3,6 +3,12 @@
 #include "nav2_costmap_2d/costmap_math.hpp"
 #include "nav2_costmap_2d/footprint.hpp"
 #include "rclcpp/parameter_events_filter.hpp"
+#include <rmw/qos_profiles.h>
+#include <rclcpp/qos.hpp>
+#include <tf2_ros/transform_listener.h>
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include <tf2_ros/buffer.h>
+
 
 using nav2_costmap_2d::LETHAL_OBSTACLE;
 using nav2_costmap_2d::INSCRIBED_INFLATED_OBSTACLE;
@@ -11,139 +17,288 @@ using nav2_costmap_2d::NO_INFORMATION;
 namespace nav2_proxemic_costmap_plugin
 {
 
-ProxemicLayer::ProxemicLayer(): last_min_x_(-std::numeric_limits<float>::max()),last_min_y_(-std::numeric_limits<float>::max()),last_max_x_(std::numeric_limits<float>::max()),last_max_y_(std::numeric_limits<float>::max()){
-}
+ProxemicLayer::~ProxemicLayer() {}
 
 void ProxemicLayer::onInitialize()
 {
     auto node = node_.lock(); //node_ (weak_ptr), node (shared_ptr)
 
-    RCLCPP_INFO(node->get_logger(),"adios\n");
+    auto qos_var = rclcpp::QoS(rclcpp::KeepLast(1), rmw_qos_profile_default);
 
-    sub_ = node->create_subscription<geometry_msgs::msg::Pose>("/people_topic",10,std::bind(&ProxemicLayer::peopleCallBack, this, std::placeholders::_1));
+    sub_ = node->create_subscription<geometry_msgs::msg::PoseArray>("/poses_topic",qos_var,std::bind(&ProxemicLayer::peopleCallBack, this, std::placeholders::_1));
 
-    // sub_ = node->create_subscription<geometry_msgs::msg::Pose>("/people_topic",
-    // rclcpp::QoS(rclcpp::KeepLast(10)).transient_local().reliable(),std::bind(&SocialLayer::setActionCallback, this, std::placeholders::_1));
-    //-------------------------------------------------------------------
-
-    declareParameter("enabled", rclcpp::ParameterValue(true));    
+    declareParameter("enabled", rclcpp::ParameterValue(true));
     node->get_parameter(name_ + "." + "enabled", enabled_);
 
     need_recalculation_ = false;
-    pose_.position.x = 0.0;
-    pose_.position.y = 0.0;
-    pose_.orientation.z = 0.0;
+    current_ = true;
+    update_cost_ = false;
 
+    global_frame_ = layered_costmap_->getGlobalFrameID();
+    rolling_window_ = false;
+
+    ProxemicLayer::matchSize();
+
+    proxemic_costmap_ = std::make_shared<Costmap2D>(
+    layered_costmap_->getCostmap()->getSizeInCellsX(),
+    layered_costmap_->getCostmap()->getSizeInCellsY(),
+    layered_costmap_->getCostmap()->getResolution(),
+    layered_costmap_->getCostmap()->getOriginX(),
+    layered_costmap_->getCostmap()->getOriginY());
+
+    proxemic_costmap_->setDefaultValue(nav2_costmap_2d::FREE_SPACE);
+
+    RCLCPP_INFO(node->get_logger(),"resolution: %f", resolution_);
+
+    global_max_x = 0.5;
+    global_max_y = 0.5;
+    global_min_x = - 0.5;
+    global_min_y = -0.5;
+    i_max = 0;
+    nuevo = false;
 }
 
-void ProxemicLayer::getFrameNames()
-{
-    // auto frames = tf_buffer_->getAllFrameNames();
-    // for (auto tf : frames) {    
-    //     if (tf.find(tf_prefix_) != std::string::npos) {
-    //     agent_ids_.push_back(tf);
-    //     }
-    // }
-    // sort(agent_ids_.begin(), agent_ids_.end());
-    // agent_ids_.erase(unique(agent_ids_.begin(), agent_ids_.end()), agent_ids_.end());
-}
-
-bool ProxemicLayer::getAgentTFs(std::vector<tf2::Transform> & agents) const
-{
-    // geometry_msgs::msg::TransformStamped global2agent;
-    // auto node = node_.lock(); //node_ (weak_ptr), node (shared_ptr)
-    // for (auto id : agent_ids_) {
-    //     try {
-    //     // Check if the transform is available
-    //     global2agent = tf_buffer_->lookupTransform(global_frame_, id, tf2::TimePointZero);
-    //     } catch (tf2::TransformException & e) {
-    //     RCLCPP_WARN(node->get_logger(), "%s", e.what());
-    //     return false;
-    //     }
-    //     tf2::Transform global2agent_tf2;
-    //     tf2::impl::Converter<true, false>::convert(global2agent.transform, global2agent_tf2);
-    //     agents.push_back(global2agent_tf2);
-    // }
-    return true;
-}
-
-void ProxemicLayer::peopleCallBack(const geometry_msgs::msg::Pose::SharedPtr msg){
-    
-    pose_.position.x = msg->position.x;
-    pose_.position.y = msg->position.y;
-    pose_.orientation.z = msg->orientation.z;
+void ProxemicLayer::peopleCallBack(const geometry_msgs::msg::PoseArray msg){
 
     auto node = node_.lock();
-    RCLCPP_INFO(node->get_logger(),"He recibido la pose: [x:%d, y:%d, o:%d]", pose_.position.x, pose_.position.y, pose_.orientation.z);
 
-    need_recalculation_ = true;
+    i_max = msg.poses.size();
+
+    RCLCPP_INFO(node->get_logger(),"He recibido %d poses. (callback)",i_max);
+
+    posesx.clear();
+    posesy.clear();
+    posesz.clear();
+
+    for (int i = 0; i < i_max; i++){
+
+        posesx.push_back(msg.poses[i].position.x);
+        posesy.push_back(msg.poses[i].position.y);
+        posesz.push_back(msg.poses[i].orientation.z);
+
+        RCLCPP_INFO(node->get_logger(),"He recibido la pose: [x:%f, y:%f, o:%f]", posesx[i], posesy[i], posesz[i]);
+    }
+
+    if(i_max > 0){
+        nuevo = true;
+
+    }else{
+        nuevo = false;
+    }
+
+    need_recalculation_ = false;
+    update_cost_ = false;
 }
 
-void ProxemicLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double * min_x, double * min_y, double * max_x, double * max_y){ 
-    
-    // ejemplo sencillo de bounds
-    if (need_recalculation_ && !enabled_){
-        *min_x = (pose_.position.x)-3;
-        *min_y = (pose_.position.y)-3;
-        *max_x = (pose_.position.x)+3;
-        *max_y = (pose_.position.y)+3;
-        need_recalculation_ = false;
+void ProxemicLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double * min_x, double * min_y, double * max_x, double * max_y){
+
+    auto node = node_.lock();
+
+    if(need_recalculation_){
+        //_________________________________________________________________DE AQUI PARA ABAJO BIEN, PERO NO ME DIBUJA EL "RASTRO"
+        // *max_x = global_max_x;
+        // *max_y = global_max_y;
+        // *min_x = global_min_x;
+        // *min_y = global_min_y;
+
+        // RCLCPP_INFO(node->get_logger(),"Dentro bounds");
+
+        // int tam = posesx.size();
+        // for (int i = 0; i < tam; i++){
+        //     if(posesx[i] > *max_x){
+        //         *max_x = posesx[i] + 2;
+        //     }
+        //     if(posesx[i] < *min_x){
+        //         *min_x = posesx[i] - 2;
+        //     }
+        //     if(posesy[i] > *max_y){
+        //         *max_y = posesy[i] + 2;
+        //     }
+        //     if(posesy[i] < *min_y){
+        //         *min_y = posesy[i] - 2;
+        //     }
+        // }
+
+        // global_max_x = *max_x;
+        // global_max_y = *max_y;
+        // global_min_x = *min_x;
+        // global_min_y = *min_y;
+        //_________________________________________________________________DE AQUI PARA ARRIBA BIEN, PERO NO ME DIBUJA EL "RASTRO"
+
+        //____________________________________DE AQUI PARA ABAJO COJO LA VENTANA ENTERA PARA VER SI ME DIBUJA EL RASTRO
+        *min_x = -6.5;
+        *min_y = -3.5;
+        *max_x = 4;
+        *max_y = 10;
+        //____________________________________DE AQUI PARA ARRIBA COJO LA VENTANA ENTERA PARA VER SI ME DIBUJA EL RASTRO
+
+        RCLCPP_INFO(node->get_logger(),"NEED RECALCULATION");
+
+        update_cost_ = true;
+
+
+    }else{
+        *min_x = -6.5;
+        *min_y = -3.5;
+        *max_x = 4;
+        *max_y = 10;
+
+        if(nuevo){
+            need_recalculation_ = true;
+            nuevo = false;
+            update_cost_ = false;
+            RCLCPP_INFO(node->get_logger(),"en el if nuevo");
+        }
+
+        RCLCPP_INFO(node->get_logger(),"NO NEED");
+
     }
+
 }
 
 void ProxemicLayer::updateCosts(nav2_costmap_2d::Costmap2D & master_grid, int min_i, int min_j, int max_i, int max_j)
 {
-    
+
     if (!enabled_) {return;}
 
-    // master_array - is a direct pointer to the resulting master_grid.
-    // master_grid - is a resulting costmap combined from all layers.
-    // By using this pointer all layers will be overwritten!
+    auto node = node_.lock();
 
-    // To work with costmap layer and merge it with other costmap layers,
-    // please use costmap_ pointer instead (this is pointer to current
-    // costmap layer grid) and then call one of updates methods:
-    // - updateWithAddition()
-    // - updateWithMax()
-    // - updateWithOverwrite()
-    // - updateWithTrueOverwrite()
+    if(update_cost_){
+
+        RCLCPP_INFO(node->get_logger(),"Dentro costs");
+
+        int tam = posesx.size();
+
+        for (int k = 0; k < tam; k++){
+
+            setGaussian(master_grid, posesx[k], posesy[k], 0.0);
+
+        }
+
+        if(i_max == 0){
+            update_cost_ = false;
+        }
+        need_recalculation_ = true;
+
+    }else{
+        int size_x = getSizeInCellsX();
+        int size_y = getSizeInCellsY();
+        unsigned char * master_array = master_grid.getCharMap();
+
+        for (int j = 0; j < size_y; j++) {
+            for (int i = 0; i < size_x; i++) {
+                unsigned int index = master_grid.getIndex(i,j);
+                int cost = getCost(index);
+                if(cost > 60){
+                    setCost(i, j, cost - 40);
+                    master_array[index] = cost - 40;  // si borro esta línea, solamente borro la lectura del laser y el inflado cuando estoy en Set Gaussian, pero va mas rápido
+
+                }else{
+                    setCost(i, j, nav2_costmap_2d::FREE_SPACE);
+                    
+                }
+
+            }
+        }
+        RCLCPP_INFO(node->get_logger(),"Borro");
+        updateWithMax(master_grid, min_i, min_j, max_i, max_j);
+    }
+}
+
+void ProxemicLayer::setGaussian(nav2_costmap_2d::Costmap2D & master_grid, double pose_x, double pose_y, double ori){
+    //______________________________________________________________________________________
+    // ----------------------------------CIRCULO--------------------------------------------
+    //______________________________________________________________________________________
+
+    //__________________________________________________________________________________DE AQUI PARA ABAJO COPIADO DE SOCIAL
+    // float r = 0.5;
+    // float alpha = 2 * M_PI;
+    // float orientation = 0.0;
+
+    // std::vector<geometry_msgs::msg::Point> points;
+    // // Loop over 32 angles around a circle making a point each time
+    // int N = 32;
+    // int it = static_cast<int>(round((N * alpha) / (2 * M_PI)));
+    // geometry_msgs::msg::Point pt;
+    // for (int i = 0; i < it; ++i) {
+    //     double angle = i * 2 * M_PI / N + orientation;
+    //     pt.x = (cos(angle) * r) + pose_x;                                                       // les sumo la pose
+    //     pt.y = (sin(angle) * r) + pose_y;
+    //     points.push_back(pt);
+    // }
+
+    // pt.x = points[0].x;
+    // pt.y = points[0].y;
+    // points.push_back(pt);
+    //__________________________________________________________________________________DE AQUI PARA ARRIBA COPIADO DE SOCIAL
+
+    //______________________________________________________________________________________
+    // ----------------------------------COSTE--------------------------------------------
+    //______________________________________________________________________________________
     
-    // In this case using master_array pointer is equal to modifying local costmap_
-    // pointer and then calling updateWithTrueOverwrite():
+    auto node = node_.lock();
 
-    //DESCOMENTAR:
+    int limit_min_i = 0;
+    int limit_min_j = 0;
+    int limit_max_i = 0;
+    int limit_max_j = 0;
+    int center_x = 0;
+    int center_y = 0;
+    int A = nav2_costmap_2d::LETHAL_OBSTACLE;
+    double sigx = 6;
+    double sigy = 6;
+
+    worldToMapEnforceBounds(pose_x - 0.5, pose_y - 0.5, limit_min_i, limit_min_j);
+    worldToMapEnforceBounds(pose_x + 0.5, pose_y + 0.5, limit_max_i, limit_max_j);
+    worldToMapEnforceBounds(pose_x, pose_y, center_x, center_y);
+
     unsigned char * master_array = master_grid.getCharMap();
-    unsigned int size_x = master_grid.getSizeInCellsX();
-    unsigned int size_y = master_grid.getSizeInCellsY();
 
-    // {min_i, min_j} - {max_i, max_j} - are update-window coordinates.
-    // These variables are used to update the costmap only within this window
-    // avoiding the updates of whole area.
-    //
-    // Fixing window coordinates with map size if necessary.
 
-    //DESCOMENTAR:
-    //min_i = std::max(0, min_i);
-    //min_j = std::max(0, min_j);
-    max_i = std::min(static_cast<int>(size_x), max_i);
-    max_j = std::min(static_cast<int>(size_y), max_j);
-
-    // Simply computing one-by-one cost per each cell
-    int gradient_index;
-    for (int j = min_j; j < max_j; j++) {
-        for (int i = min_i; i < max_i; i++) {
+    for (int j = limit_min_j; j < limit_max_j+1; j++) {
+        for (int i = limit_min_i; i < limit_max_i+1; i++) {
+            unsigned int cost = round(A*exp(-((pow((i - center_x),2)/(2*pow(sigx,2)))+(pow((j - center_y),2)/(2*pow(sigy,2))))));
             int index = master_grid.getIndex(i, j);
-            // setting the gradient cost
-            unsigned char cost = LETHAL_OBSTACLE;
-            costmap_[index] = cost;
+
+            if(cost > 60){
+                setCost(i,j,cost);
+                master_array[index] = cost;
+            }
         }
     }
 
-    updateWithAddition(master_grid, min_i, min_j, max_i, max_j);
+    //updateWithOverwrite(master_grid, limit_min_i, limit_min_j, limit_max_i, limit_max_j);
 
-}
+    //______________________________________________________________________________________
+    // ----------------------------------BOUNDS--------------------------------------------
+    //______________________________________________________________________________________
 
-void ProxemicLayer::matchSize(){
+    //_________________________________________________________________DE AQUI PARA ABAJO BIEN, PERO NO ME DIBUJA EL "RASTRO"
+    // int map_x = 0;
+    // int map_y = 0;
+
+    // worldToMapEnforceBounds(pose_x, pose_y, map_x, map_y);
+
+    // int max_i = map_x + (r/resolution_) + 2;
+    // int max_j = map_y + (r/resolution_) + 2;
+    // int min_i = map_x - (r/resolution_) - 1;
+    // int min_j = map_y - (r/resolution_) - 1;
+    //_________________________________________________________________DE AQUI PARA ARRIBA BIEN, PERO NO ME DIBUJA EL "RASTRO"
+
+
+    //____________________________________DE AQUI PARA ABAJO COJO LA VENTANA ENTERA PARA VER SI ME DIBUJA EL RASTRO
+    int map_min_i = 0;
+    int map_min_j = 0;
+    int map_max_i = 0;
+    int map_max_j = 0;
+
+    worldToMapEnforceBounds(-6.5, -3.5, map_min_i, map_min_j);
+    worldToMapEnforceBounds(4, 10, map_max_i, map_max_j);
+
+    //____________________________________DE AQUI PARA ARRIBA COJO LA VENTANA ENTERA PARA VER SI ME DIBUJA EL RASTRO
+
+    updateWithMax(master_grid, map_min_i, map_min_j, map_max_i, map_max_j);
+    
 
 }
 
@@ -159,3 +314,5 @@ bool ProxemicLayer::isClearable() {return false;}
 
 #include "pluginlib/class_list_macros.hpp"
 PLUGINLIB_EXPORT_CLASS(nav2_proxemic_costmap_plugin::ProxemicLayer, nav2_costmap_2d::CostmapLayer)
+PLUGINLIB_EXPORT_CLASS(nav2_proxemic_costmap_plugin::ProxemicLayer, nav2_costmap_2d::Layer)
+PLUGINLIB_EXPORT_CLASS(nav2_proxemic_costmap_plugin::ProxemicLayer, nav2_costmap_2d::Costmap2D)
